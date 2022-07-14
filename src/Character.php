ef382320df087c4ed2ed0d248ab5091f47773407
Tania Rodríguez Fernández <taniacsif@gmail.com>
2022-07-14 09:50:33 +0200

    pikachuCharacter

--- src/Character.php
+++ src/Character.php
@@ -2,10 +2,37 @@
 
 namespace App;
 
+use phpDocumentor\Reflection\Types\Boolean;
+
 class Character {
 
+    private int $health;
+    private int $level;
+    private bool $live;
+    private int $damageRecived;
+    public function __construct()
+    {
+        $this->health = 1000;
+        $this->level= 1;
+        $this->live= true;
+        $this->damageRecived= 500;
+    }
+    public function getHealth()
+    {
+        return $this->health;
+    }
     
-    
+    public function getLevel()
+    {
+        return $this->level;
+    }
+    public function getLive()
+    {
+        return $this->live;
+    }
+    public function getDamageRecived()
+    {
+        return $this->damageRecived;
+    }
 }
-
-?>
+//◦ Damage is subtracted from Health
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -9,11 +9,53 @@ use App\Faction;
 class CharacterTest extends TestCase {
 
 	/** @test */
-	public function test_function_example()
+	public function test_character_health_starting_at_1000_points()
 	{
-		$this->assertEquals(4, 4);
+		//given
+		$goku = new Character;
+		$pikachu = new Character;
+		//when
+		$gokuHealth = $goku->getHealth();
+		$pikachuHealth = $pikachu->getHealth(1000);
+		//then
+		$this->assertEquals(1000,$gokuHealth);
+		$this->assertEquals(1000,$pikachuHealth);
 	}
+
 	
+	public function test_character_level_starting_at_one()
+	{
+			//given
+			$goku = new Character;
+			$pikachu = new Character;
+			//when
+			$gokuLevel = $goku->getLevel();
+			$pikachuLevel = $pikachu->getLevel();
+			//then
+			$this->assertEquals(1,$gokuLevel);
+			$this->assertEquals(1,$pikachuLevel);
+	}
+	public function test_character_is_alive_or_dead()
+	{
+			//given
+			$goku = new Character;
+			$pikachu = new Character;
+			//when
+			$gokuLive = $goku->getLive();
+			$pikachuLive = $pikachu->getLive();
+			//then
+			$this->assertEquals(true,$gokuLive);
+			$this->assertEquals(true,$pikachuLive);
+	}
+	public function test_damage_recived()
+	{
+			//given
+			$goku = new Character;
+			//when
+			$gokuDamageRecived = $goku->getDamageRecived();
+			//then
+			$this->assertEquals(true,$gokuDamageRecived);
+	}
 
 }
 
